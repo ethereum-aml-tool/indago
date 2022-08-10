146d000495227adc08878d37f6bca26a24ce6398
ponbac <root@ponbac.xyz>
2022-08-10 17:00:25 +0200

    removed commented code

--- scripts/gcs/download_and_sort.py
+++ scripts/gcs/download_and_sort.py
@@ -23,36 +23,36 @@ DTYPES = {'block_number': int, 'transaction_index': pd.Int32Dtype(), 'from_addre
 # 'None' = Download all blobs to local storage.
 MAX_BLOBS: Optional[int] = None
 
-# STEP_SIZE: int = 50
-# START_OFFSET: int = 0 #6000 #12000 #18000
-# END_OFFSET: int = START_OFFSET + STEP_SIZE
-# N_BLOBS: int = 6000 #12000 #18000 #23478
-# while END_OFFSET < N_BLOBS + STEP_SIZE:
-#     print('[DOWNLOADING]')
-#     storage: EthereumStorage = GoogleCloudStorage()
-#     storage.download(BUCKET_NAME, f'{BQ_TABLE_NAME}/',
-#                      f'{DOWNLOAD_DIR}/{BQ_TABLE_NAME}', MAX_BLOBS, use_cols=COLUMNS_TO_SAVE, dtypes=DTYPES, start_offset=START_OFFSET, end_offset=END_OFFSET)
+STEP_SIZE: int = 50
+START_OFFSET: int = 0 #6000 #12000 #18000
+END_OFFSET: int = START_OFFSET + STEP_SIZE
+N_BLOBS: int = 6000 #12000 #18000 #23478
+while END_OFFSET < N_BLOBS + STEP_SIZE:
+    print('[DOWNLOADING]')
+    storage: EthereumStorage = GoogleCloudStorage()
+    storage.download(BUCKET_NAME, f'{BQ_TABLE_NAME}/',
+                     f'{DOWNLOAD_DIR}/{BQ_TABLE_NAME}', MAX_BLOBS, use_cols=COLUMNS_TO_SAVE, dtypes=DTYPES, start_offset=START_OFFSET, end_offset=END_OFFSET)
 
-#     print('\n[SORTING]')
-#     COLUMN_TO_SORT: int = 1
-#     SEC_COLUMN_TO_SORT: int = 2
-#     CORES: int = os.cpu_count()
-#     for file in tqdm(os.listdir(f'{DOWNLOAD_DIR}/{BQ_TABLE_NAME}')):
-#         path: str = f'{DOWNLOAD_DIR}/{BQ_TABLE_NAME}/{file}'
-#         if subprocess.call(f"tail -n+2 {path} | LC_ALL=C sort -t',' --parallel={CORES} -k {COLUMN_TO_SORT},{COLUMN_TO_SORT}n -k {SEC_COLUMN_TO_SORT},{SEC_COLUMN_TO_SORT}n > {DOWNLOAD_DIR}/{BQ_TABLE_NAME}/sorted-{file}", shell=True) != 0:
-#             print(f'ERROR: Failed to sort {file} {BQ_TABLE_NAME}')
-#             sys.exit(1)
-#         os.remove(f'{DOWNLOAD_DIR}/{BQ_TABLE_NAME}/{file}')
+    print('\n[SORTING]')
+    COLUMN_TO_SORT: int = 1
+    SEC_COLUMN_TO_SORT: int = 2
+    CORES: int = os.cpu_count()
+    for file in tqdm(os.listdir(f'{DOWNLOAD_DIR}/{BQ_TABLE_NAME}')):
+        path: str = f'{DOWNLOAD_DIR}/{BQ_TABLE_NAME}/{file}'
+        if subprocess.call(f"tail -n+2 {path} | LC_ALL=C sort -t',' --parallel={CORES} -k {COLUMN_TO_SORT},{COLUMN_TO_SORT}n -k {SEC_COLUMN_TO_SORT},{SEC_COLUMN_TO_SORT}n > {DOWNLOAD_DIR}/{BQ_TABLE_NAME}/sorted-{file}", shell=True) != 0:
+            print(f'ERROR: Failed to sort {file} {BQ_TABLE_NAME}')
+            sys.exit(1)
+        os.remove(f'{DOWNLOAD_DIR}/{BQ_TABLE_NAME}/{file}')
 
-#     print('\n[UPLOADING]')
-#     if subprocess.call(['gsutil', '-m', 'cp', '-r', f'{DOWNLOAD_DIR}/{BQ_TABLE_NAME}', f'gs://{BUCKET_NAME}/pruned/']) == 0:
-#         print(f'Uploaded pruned {BQ_TABLE_NAME}!')
-#         rmtree(f'{DOWNLOAD_DIR}/{BQ_TABLE_NAME}')
-#         START_OFFSET += STEP_SIZE
-#         END_OFFSET += STEP_SIZE
-#     else:
-#         print(f'ERROR: Failed to upload {BQ_TABLE_NAME}')
-#         sys.exit(1)
+    print('\n[UPLOADING]')
+    if subprocess.call(['gsutil', '-m', 'cp', '-r', f'{DOWNLOAD_DIR}/{BQ_TABLE_NAME}', f'gs://{BUCKET_NAME}/pruned/']) == 0:
+        print(f'Uploaded pruned {BQ_TABLE_NAME}!')
+        rmtree(f'{DOWNLOAD_DIR}/{BQ_TABLE_NAME}')
+        START_OFFSET += STEP_SIZE
+        END_OFFSET += STEP_SIZE
+    else:
+        print(f'ERROR: Failed to upload {BQ_TABLE_NAME}')
+        sys.exit(1)
 
 subprocess.call(['gsutil', '-m', 'cp', '-r',
                     f'gs://{BUCKET_NAME}/pruned/', DOWNLOAD_DIR])
