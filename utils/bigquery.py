53ce939798b318c0243ee93614a23be10ffd7760
ponbac <root@ponbac.xyz>
2024-02-05 20:41:57 +0100

    updated stuff

--- utils/bigquery.py
+++ utils/bigquery.py
@@ -15,11 +15,11 @@ Here is a list of all the tables:
     bigquery-public-data.crypto_ethereum.traces
     bigquery-public-data.crypto_ethereum.transactions
 """
+
 import os
 import pandas as pd
 from typing import List, Any, Optional, Tuple, Set
-from google.cloud.bigquery import (
-    Client, DatasetReference, DestinationFormat, job)
+from google.cloud.bigquery import Client, DatasetReference, DestinationFormat, job
 from google.api_core.page_iterator import HTTPIterator
 from google.cloud.bigquery.table import RowIterator
 
@@ -29,35 +29,39 @@ class EthereumBigQuery:
     Wrapper class to copy Ethereum blockchain data from BigQuery to buckets.
     """
 
-    def __init__(self, dataset_id: str = 'bigquery-public-data.crypto_ethereum'):
+    def __init__(self, dataset_id: str = "bigquery-public-data.crypto_ethereum"):
         self.client: Client = Client()
-        assert len(os.environ['GOOGLE_APPLICATION_CREDENTIALS']) > 0, \
-            "Set GOOGLE_APPLICATION_CREDENTIALS prior to use."
+        assert (
+            len(os.environ["GOOGLE_APPLICATION_CREDENTIALS"]) > 0
+        ), "Set GOOGLE_APPLICATION_CREDENTIALS prior to use."
         self.dataset_id: str = dataset_id
 
     def get_table_names(self) -> List[str]:
         tables: List[HTTPIterator] = self.client.list_tables(self.dataset_id)
         names: List[str] = []
         for table in tables:
-            name: str = '{}.{}.{}'.format(
-                table.project, table.dataset_id, table.table_id)
+            name: str = "{}.{}.{}".format(
+                table.project, table.dataset_id, table.table_id
+            )
             names.append(name)
         return names
 
-    def export_to_bucket(self, table: str, bucket: str, as_json: bool = False, as_parquet: bool = False) -> None:
+    def export_to_bucket(
+        self, table: str, bucket: str, as_json: bool = False, as_parquet: bool = False
+    ) -> None:
         """
         Export a full table to a Google Cloud bucket.
         """
-        # wildcard notation: 
+        # wildcard notation:
         # https://cloud.google.com/bigquery/docs/exporting-data#exporting_data_into_one_or_more_files
         # https://github.com/googleapis/python-bigquery/blob/35627d145a41d57768f19d4392ef235928e00f72/docs/snippets.py
-        extension: str = 'csv'
+        extension: str = "csv"
         if as_json:
-            extension = 'json'
+            extension = "json"
         elif as_parquet:
-            extension = 'parquet'
-        destination_uri: str = f'gs://{bucket}/{table}-*.{extension}'
-        project, dataset = self.dataset_id.split('.')
+            extension = "parquet"
+        destination_uri: str = f"gs://{bucket}/{table}-*.{extension}"
+        project, dataset = self.dataset_id.split(".")
         dataset_ref: DatasetReference = DatasetReference(project, dataset)
         table_ref: DatasetReference = dataset_ref.table(table)
 
@@ -69,7 +73,7 @@ class EthereumBigQuery:
                 table_ref,
                 destination_uri,
                 job_config=job_config,
-                location='US',
+                location="US",
             )
         elif as_parquet:
             job_config = job.ExtractJobConfig()
@@ -79,13 +83,13 @@ class EthereumBigQuery:
                 table_ref,
                 destination_uri,
                 job_config=job_config,
-                location='US',
+                location="US",
             )
         else:
             extract_job: Any = self.client.extract_table(
                 table_ref,
                 destination_uri,
-                location='US',
+                location="US",
             )
 
         extract_job.result()
