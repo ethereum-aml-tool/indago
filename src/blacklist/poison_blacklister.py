31927e430872917298560489ec46a5be0a4210aa
ponbac <root@ponbac.xyz>
2024-03-10 15:07:18 +0100

    can run

--- src/blacklist/poison_blacklister.py
+++ src/blacklist/poison_blacklister.py
@@ -34,19 +34,25 @@ class Poison(BaseBlacklist):
         self.initial_blacklist = initial_blacklist
         self.save_dir = save_dir
 
-    def make_blacklist(self, start_block: Optional[int] = None, end_block: Optional[int] = None):
+    def make_blacklist(
+        self, start_block: Optional[int] = None, end_block: Optional[int] = None
+    ):
         blacklist = pd.Series(
-            [True] * len(self.initial_blacklist), self.initial_blacklist).to_dict()
+            [True] * len(self.initial_blacklist), self.initial_blacklist
+        ).to_dict()
         n_preflagged = len(blacklist)
 
         # TODO Add progress bar (tqdm)?
         chunk_counter = 0
         chunk_size = 10000
         start_time = datetime.now()
-        for tx_chunk in self.data_loader.yield_traces(chunk_size, start_block, end_block):
+        for tx_chunk in self.data_loader.yield_traces(
+            chunk_size, start_block, end_block
+        ):
             pd.options.mode.chained_assignment = None
-            nonzero_traces = tx_chunk[(tx_chunk.value.astype(int) > 0) & (
-                tx_chunk.status == 1)].dropna(subset=["to_address", "value"])
+            nonzero_traces = tx_chunk[
+                (tx_chunk.value > "0") & (tx_chunk.status == 1)
+            ].dropna(subset=["to_address", "value"])
             nonzero_traces = nonzero_traces.reset_index()
             nonzero_traces = nonzero_traces.drop("index", axis=1)
             from_addresses = nonzero_traces.iloc[:, 3]
@@ -54,7 +60,7 @@ class Poison(BaseBlacklist):
 
             for i in range(len(nonzero_traces)):
                 from_blacklisted = blacklist.get(from_addresses[i])
-                if (from_blacklisted):
+                if from_blacklisted:
                     blacklist[to_addresses[i]] = True
 
             chunk_counter += 1
@@ -62,41 +68,45 @@ class Poison(BaseBlacklist):
                 rows_processed = "{:,}".format(chunk_counter * chunk_size)
                 n_blacklisted = "{:,}".format(len(blacklist) - n_preflagged)
                 max_block = nonzero_traces.block_number.max()
-                processed_after = (datetime.now() - start_time)
+                processed_after = datetime.now() - start_time
                 ram_usage = round(psutil.virtual_memory().used / (1000**3), 2)
 
                 print(
-                    f'\nProcessed chunk {chunk_counter}, ~{rows_processed} total transactions.')
-                print(f'{n_blacklisted} addresses blacklisted.')
-                print(
-                    f'Reached block {max_block}.')
-                print(f'RAM usage: {ram_usage} GB.')
-                print(f'{processed_after} time elapsed.')
-                print('Working...')
+                    f"\nProcessed chunk {chunk_counter}, ~{rows_processed} total transactions."
+                )
+                print(f"{n_blacklisted} addresses blacklisted.")
+                print(f"Reached block {max_block}.")
+                print(f"RAM usage: {ram_usage} GB.")
+                print(f"{processed_after} time elapsed.")
+                print("Working...")
 
                 # log these stats to csv
                 run_data = {
-                    'chunk': chunk_counter,
-                    'rows_processed': int(rows_processed.replace(',', '')),
-                    'n_blacklisted': int(n_blacklisted.replace(',', '')),
-                    'max_block': max_block,
-                    'processed_after': processed_after,
-                    'ram_usage_gb': ram_usage
+                    "chunk": chunk_counter,
+                    "rows_processed": int(rows_processed.replace(",", "")),
+                    "n_blacklisted": int(n_blacklisted.replace(",", "")),
+                    "max_block": max_block,
+                    "processed_after": processed_after,
+                    "ram_usage_gb": ram_usage,
                 }
                 run_data: pd.DataFrame = pd.DataFrame.from_dict([run_data])
                 if chunk_counter == 1:
-                    run_data.to_csv(
-                        f'{self.save_dir}/poison-rundata.csv', index=False)
+                    run_data.to_csv(f"{self.save_dir}/poison-rundata.csv", index=False)
                 else:
                     run_data.to_csv(
-                        f'{self.save_dir}/poison-rundata.csv', mode='a', header=False, index=False)
+                        f"{self.save_dir}/poison-rundata.csv",
+                        mode="a",
+                        header=False,
+                        index=False,
+                    )
 
-        print(f'\nSaving results to {self.save_dir}/poison-result.csv...')
+        print(f"\nSaving results to {self.save_dir}/poison-result.csv...")
         if not os.path.exists(os.path.dirname(self.save_dir)):
             os.makedirs(os.path.dirname(self.save_dir))
-        dict_to_csv(blacklist, ['address', 'flagged'],
-                    f'{self.save_dir}/poison-result.csv')
+        dict_to_csv(
+            blacklist, ["address", "flagged"], f"{self.save_dir}/poison-result.csv"
+        )
 
         end_time = datetime.now()
         time_taken = end_time - start_time
-        print('\nFinished! Total elapsed time: ', time_taken)
+        print("\nFinished! Total elapsed time: ", time_taken)
