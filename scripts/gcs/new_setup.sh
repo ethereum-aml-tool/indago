e7ecc4f05e86ec89665544c3a341e9eecc26c95b
ponbac <root@ponbac.xyz>
2024-03-24 11:44:46 +0100

    upd

--- scripts/gcs/new_setup.sh
+++ scripts/gcs/new_setup.sh
@@ -1,9 +1,10 @@
 mkdir data
 mkdir data/traces
+sudo apt update
 echo 'export PYTHONPATH=$PYTHONPATH:~/indago' >> ~/.bashrc
 source ~/.bashrc
 cd indago
-sudo apt install python3-pip
+sudo apt install python3-pip -y
 pip install --no-cache-dir --upgrade -r requirements.txt
 
 # git clone https://github.com/ethereum-aml-tool/indago.git
