823c67d177a8409ad3406da7b36c46503a365990
ponbac <root@ponbac.xyz>
2024-03-24 09:44:25 +0100

    mb

--- scripts/gcs/new_setup.sh
+++ scripts/gcs/new_setup.sh
@@ -1,5 +1,7 @@
 mkdir data
 git clone https://github.com/ethereum-aml-tool/indago.git
+echo 'export PYTHONPATH=$PYTHONPATH:~/indago' >> ~/.bashrc
+source ~/.bashrc
 cd indago
 sudo apt install python3-pip
 pip install --no-cache-dir --upgrade -r requirements.txt
--- utils/storage/google_cloud_storage.py
+++ utils/storage/google_cloud_storage.py
@@ -31,7 +31,17 @@ class GoogleCloudStorage(EthereumStorage):
         return bucket.blob(file).exists()
 
     # TODO Parallelize this.
-    def download(self, bucket: str, dir_path: str, out_dir: str, max_blobs: Optional[int] = None, use_cols: Optional[List[str]] = None, dtypes: Optional[Dict] = None, start_offset: Optional[int] = None, end_offset: Optional[int] = None) -> None:
+    def download(
+        self,
+        bucket: str,
+        dir_path: str,
+        out_dir: str,
+        max_blobs: Optional[int] = None,
+        use_cols: Optional[List[str]] = None,
+        dtypes: Optional[Dict] = None,
+        start_offset: Optional[int] = None,
+        end_offset: Optional[int] = None,
+    ) -> None:
         bucket: Bucket = self.client.get_bucket(bucket)
         if not os.path.isdir(out_dir):
             os.makedirs(out_dir)
@@ -44,20 +54,31 @@ class GoogleCloudStorage(EthereumStorage):
         elif end_offset is not None:
             blobs = blobs[:end_offset]
 
-        for i in tqdm(range(len(blobs) if max_blobs is None else min(max_blobs, len(blobs)))):
+        for i in tqdm(
+            range(len(blobs) if max_blobs is None else min(max_blobs, len(blobs)))
+        ):
             blob: Blob = blobs[i]
-            blob.download_to_filename(os.path.join(out_dir, blob.name.split('/')[-1]))
+            full_path: str = os.path.join(out_dir, blob.name.split("/")[-1])
+            print(f"Downloading {blob.name} to {full_path}")
+            blob.download_to_filename(full_path)
             if use_cols is not None:
-                pd.read_csv(os.path.join(out_dir, blob.name.split('/')[-1]), usecols=use_cols, dtype=dtypes)[use_cols].to_csv(os.path.join(out_dir, blob.name.split('/')[-1]), index=False)
+                pd.read_csv(
+                    full_path,
+                    usecols=use_cols,
+                    dtype=dtypes,
+                )[
+                    use_cols
+                ].to_csv(full_path, index=False)
 
     def upload(self, blob_name: str, path_to_file: str, bucket: str) -> str:
         bucket: Bucket = self.client.get_bucket(bucket)
         blob: Blob = bucket.blob(blob_name)
         blob.upload_from_filename(path_to_file)
-        
+
         return blob.public_url
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     storage: EthereumStorage = GoogleCloudStorage()
-    print(storage.list_blobs('eth-aml-data', 'sorted/'))
+    print(storage.list_blobs("eth-aml-data", "sorted/"))
     pass
