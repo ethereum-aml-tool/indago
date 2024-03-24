9c32e30e7e907f6053ba97acf11783f9a429664d
ponbac <root@ponbac.xyz>
2024-03-24 11:19:07 +0100

    r

--- scripts/gcs/new_setup.sh
+++ scripts/gcs/new_setup.sh
@@ -6,3 +6,5 @@ source ~/.bashrc
 cd indago
 sudo apt install python3-pip
 pip install --no-cache-dir --upgrade -r requirements.txt
+
+# nohup python3 scripts/gcs/download_and_sort.py &> /home/ponbac/out.txt &
