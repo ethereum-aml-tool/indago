c90cc273c1c085e7bcdff1676162920a577b3b81
ponbac <root@ponbac.xyz>
2024-03-24 11:39:40 +0100

    1

--- scripts/gcs/new_setup.sh
+++ scripts/gcs/new_setup.sh
@@ -1,10 +1,10 @@
 mkdir data
 mkdir data/traces
-git clone https://github.com/ethereum-aml-tool/indago.git
 echo 'export PYTHONPATH=$PYTHONPATH:~/indago' >> ~/.bashrc
 source ~/.bashrc
 cd indago
 sudo apt install python3-pip
 pip install --no-cache-dir --upgrade -r requirements.txt
 
+# git clone https://github.com/ethereum-aml-tool/indago.git
 # nohup python3 scripts/gcs/download_and_sort.py &> /home/ponbac/out.txt &
