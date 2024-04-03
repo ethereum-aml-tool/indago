bad924a5f34781540ed2d7cd837c1279a7d6dff8
ponbac <root@ponbac.xyz>
2024-04-03 18:29:06 +0200

    btr

--- v2/src/run_data.rs
+++ v2/src/run_data.rs
@@ -44,10 +44,11 @@ impl fmt::Display for RunData {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         write!(
             f,
-            "Rows processed: {}\nBlacklisted addresses: {}\nProcessed after: {:.2} minutes\nRAM usage: {:.2} GB",
+            "Rows processed: {}\nBlacklisted addresses: {}\nProcessed after: {:.2} minutes\nBlock: {}\nRAM usage: {:.2} GB",
             self.rows_processed.separate_with_commas(),
             self.n_blacklisted.separate_with_commas(),
             self.processed_after.as_secs_f32() / 60.0,
+            self.current_block,
             self.ram_usage_gb
         )
     }
