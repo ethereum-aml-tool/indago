96d4606c1dc82d9f91cab0e66b7a3cb8196008f8
ponbac <root@ponbac.xyz>
2024-04-16 23:44:11 +0200

    btr

--- v2/run.sh
+++ v2/run.sh
@@ -1,6 +1,11 @@
 cargo build --release
 
 ./target/release/v2 -a seniority -d tornado 
-# ./target/release/v2 -a seniority -d known-addresses 
-
+./target/release/v2 -a seniority -d known-addresses 
+./target/release/v2 -a seniority -d combined 
 gsutil -m cp -r /data/blacklist/seniority-* gs://indago/blacklist/seniority
+
+./target/release/v2 -a haircut -d tornado
+./target/release/v2 -a haircut -d known-addresses
+./target/release/v2 -a haircut -d combined
+gsutil -m cp -r /data/blacklist/haircut-* gs://indago/blacklist/haircut
